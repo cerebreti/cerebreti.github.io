e8683c8d3495c4023ce3dd780a57e9327b35e5e5
cerebreti <74846877+cerebreti@users.noreply.github.com>
2020-11-22 16:28:35 -0600

    Create preguntas.js

---
+++ js/preguntas.js
@@ -0,0 +1,58 @@
+var questions = [{
+	"question": "¿Quién escribió la Odisea?",
+	"opcion1": "Homero",
+	"opcion2": "Virgilio",
+	"opcion3": "Dante Alighieri",
+	"opcion4": "Valmiki",
+	"respuesta": "1"
+}, {
+	"question": "¿Cuál es el río más largo del mundo?",
+	"opcion1": "Río Nilo",
+	"opcion2": "Río Misisipi",
+	"opcion3": "Río Amazonas",
+	"opcion4": "Río Obi",
+	"respuesta": "3"
+}, {
+	"question": "¿En que año se acabó la II Guerra Mundial?",
+	"opcion1": "1942",
+	"opcion2": "1943",
+	"opcion3": "1944",
+	"opcion4": "1945",
+	"respuesta": "4"
+}, {
+	"question": "¿En qué país se encuentra la torre de Pisa?",
+	"opcion1": "Austria",
+	"opcion2": "Italia",
+	"opcion3": "Holanda",
+	"opcion4": "Alemania",
+	"respuesta": "2"
+} , {
+	"question": "¿Cuál es el disco más vendido de la historia?",
+	"opcion1": "Back in Black. de AC/DC",
+	"opcion2": "Thriller, de Michael Jackson",
+	"opcion3": "The Dark Side of the Moon, de Pink Floyd.",
+	"opcion4": "El Guardaespaldas, BSO",
+	"respuesta": "2"
+},{
+	"question": "¿Cuál es el país mas grande del mundo?",
+	"opcion1": "China",
+	"opcion2": "Rusia",
+	"opcion3": "Estados Unidos",
+	"opcion4": "India",
+	"respuesta": "2"
+}, {
+	"question": "¿Cómo se llama el himno nacional de Francia?",
+	"opcion1": "Marcha Real",
+	"opcion2": "The Star-Spangled Banner",
+	"opcion3": "Deutschlandlied",
+	"opcion4": "La Marsellesa",
+	"respuesta": "4"
+} , {
+	"question": "¿Cuántos años tiene un lustro?",
+	"opcion1": "5",
+	"opcion2": "10",
+	"opcion3": "50",
+	"opcion4": "100",
+	"respuesta": "1"
+}
+];
