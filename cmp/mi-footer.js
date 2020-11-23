e35fd6caa4447c6bca289b2acb7e29d483d8d09f
cerebreti <74846877+cerebreti@users.noreply.github.com>
2020-11-22 18:09:42 -0600

    Delete mi-footer.js

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = /* html */
-      `Copyright &copy; 2020 Brayan José Cruz Robles.`;
-  }
-}
-customElements.define("mi-footer", MiFooter);
